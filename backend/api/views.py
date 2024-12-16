f6c3203766ac4522fef497bbedf9d14ea5dddd76
Mohd Ziyad <mziyadc@gmail.com>
2024-12-16 19:27:35 +0530

    added notes get and  creation

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from api.serializers import UserSerializer
+from api.serializers import NoteSerializer, UserSerializer
+from backend.api.models import Note
 
 
 # Create your views here.
@@ -11,3 +12,18 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class NoteListCreate(generics.ListAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+        else:
+            print(serializer.errors)
