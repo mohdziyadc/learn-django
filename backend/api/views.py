237c7667c83e3f02874844d91fca96512ab301ad
Mohd Ziyad <mziyadc@gmail.com>
2024-12-16 17:35:40 +0530

    [AUTH] register user added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from api.serializers import UserSerializer
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    # letting the view know to interact with which object in the db
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
