fedc0c50b48f84697a847954051a5c25bdd59e95
Mohd Ziyad <mziyadc@gmail.com>
2024-12-17 16:49:17 +0530

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2 className="text-5xl font-bold">If I still bleeeeeeed!</h2>
+    </>
+  );
+}
+
+export default App;
